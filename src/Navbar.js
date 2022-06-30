0ff3e0a3538e3190c8e4cf1548583effd563fb57
Jhamilchar <jhamil.arroyo.a@gmail.com>
2022-06-30 00:21:05 -0500

    Confused

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,18 +3,14 @@ import MenuItems from "./components/MenuItems";
 
 const Navbar = () => {
     return (
-     <nav>
-      <ul className="menus">
-        {menuItems.map((menu, index) => {
-            return(
-                <li className="menu-items" key={index}>
-                    <a href="/#">{menu.title}</a>
-                </li>
-            );
-        })}
-      </ul>
-     </nav>
-    );
-   };
+        <nav>
+         <ul className="menus">
+          {menuItems.map((menu, index) => {
+           return <MenuItems items={menu} key={index} />;
+          })}
+         </ul>
+        </nav>
+       );
+      };
    
 export default Navbar;
--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,14 +1,14 @@
-import Dropdown from "./Dropdown.js";
+// import Dropdown from "./Dropdown";
 
 const MenuItems = ({ items }) => {
  return (
   <li className="menu-items">
    {items.submenu ? (
     <>
-     <button type="button">
+     <button type="button" aria-haspopup="menu">
       {items.title}{" "}
      </button>
-     <Dropdown submenus={items.submenu} />
+     {/* <Dropdown submenus={items.submenu} /> */}
     </>
    ) : (
     <a href="/#">{items.title}</a>
